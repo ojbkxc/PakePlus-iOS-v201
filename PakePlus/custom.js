09e40d956a5dfde1829cdbad54017bed3761297d
xc <48278853+ojbkxc@users.noreply.github.com>
2025-10-07 18:45:31 +0800

    update custom js from pakeplus

--- PakePlus/custom.js
+++ PakePlus/custom.js
@@ -1,10 +1,5 @@
-console.log(
-    '%cbuild from PakePlus： https://github.com/Sjj1024/PakePlus',
-    'color:orangered;font-weight:bolder'
-)
-
 // very important, if you don't know what it is, don't touch it
-// 非常重要，不懂代码不要动
+// 非常重要，不懂代码不要动，这里可以解决80%的问题，也可以生产1000+的bug
 const hookClick = (e) => {
     const origin = e.target.closest('a')
     const isBaseTargetBlank = document.querySelector(
@@ -21,6 +16,11 @@ const hookClick = (e) => {
     } else {
         console.log('not handle origin', origin)
     }
+}
+
+window.open = function (url, target, features) {
+    console.log('open', url, target, features)
+    location.href = url
 }
 
 document.addEventListener('click', hookClick, { capture: true })
